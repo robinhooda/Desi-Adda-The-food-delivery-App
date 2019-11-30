ada3904340f11ed875c5dfda42ab9d8e1dd7c416
Robin Hooda <robinhooda66@gmail.com>
2019-11-30 15:22:43 +0530

    homescreen component done

---
+++ client/src/component/homeScreen/home.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class home extends React.Component{
+
+ render(){
+     return(
+          <div className="home">
+          <h1>Hello</h1>
+          </div>
+     );
+ }
+
+};
+
+
+export default home;
